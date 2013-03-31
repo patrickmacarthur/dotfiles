8424c1c005d1ed5e48c42fc62e1ce1cd0308f8db
Patrick MacArthur <generalpenguin89@gmail.com>
2013-03-31 11:22:46 -0400

    Simplify and fix bugs in xmonad/xmobar config

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,32 +1,21 @@
 -- File: .xmonad/xmonad.hs
 
--- Based on xmonad example config file for xmonad-0.9
+-- Based on xmonad example config file for xmonad 0.9+
 --
 -- Run xmonad --recompile or enter Super/Meta-Q to compile this configuration
 -- file.
 --
 import XMonad
-import XMonad.Config.Desktop
 import XMonad.Layout.Grid
 import XMonad.Layout.IM
-import XMonad.Layout.LayoutModifier
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Reflect
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks (manageDocks,avoidStruts,AvoidStruts)
 import XMonad.Hooks.ManageHelpers (isFullscreen,doFullFloat)
 import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Util.Loggers
-import XMonad.Util.Run
 import Graphics.X11.ExtraTypes.XF86
-import Control.Monad ((=<<))
-import Data.Monoid
 import Data.Ratio ((%))
-import System.IO (hPutStrLn)
-
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
 
 -- mod1Mask is left-Alt, mod3Mask is right-Alt,
 -- mod4Mask is Super (also referred to by a certain registered trademark)
@@ -52,42 +41,77 @@ myWorkspaces = ["1:xterm", "2:web", "3:mail", "4:music", "5:chat", "6:gimp", "7"
 --
 myManageHook = composeAll $
         [ className =? "MPlayer"        --> doFloat
-        , className =? "Gimp"           --> doFloat
         , className =? "Xmessage"       --> doFloat
         , className =? "Zenity"         --> doFloat
-        , className =? "Pidgin"         --> doShift "3:chat"
-        , resource =? "empathy"         --> doShift "3:chat"
-        , className =? "Empathy"        --> doShift "3:chat"
+        , className =? "Pidgin"         --> doShift "5:chat"
+        , resource =? "empathy"         --> doShift "5:chat"
+        , className =? "Empathy"        --> doShift "5:chat"
         , className =? "Firefox"        --> doShift "2:web"
         , (className =? "Firefox" <&&> fmap (/= "Navigator") appName) --> doFloat
-        , className =? "Thunderbird"    --> doShift "5:mail"
+        , className =? "Thunderbird"    --> doShift "3:mail"
         , (className =? "Thunderbird" <&&> className =? "Dialog")    --> doFloat
 	, stringProperty "WM_WINDOW_ROLE" =? "AlarmWindow"     --> doFloat
         , className =? "Amarok"         --> doShift "4:music"
-        , className =? "ario"         --> doShift "4:music"
+        , className =? "rhythmbox"      --> doShift "4:music"
+        , className =? "ario"           --> doShift "4:music"
         , resource  =? "desktop_window" --> doIgnore
         , resource  =? "kdesktop"       --> doIgnore 
         , isFullscreen                  --> doFullFloat
         ]
 
-myLayoutHook = avoidStruts $ onWorkspace "5:chat" imLayout $
+------------------------------------------------------------------------
+-- Layouts:
+
+-- You can specify and transform your layouts by modifying these values.
+-- If you change layout bindings be sure to use 'mod-shift-space' after
+-- restarting (with 'mod-q') to reset your layout state to the new
+-- defaults, as xmonad preserves your old layout settings by default.
+--
+-- The available layouts.  Note that each layout is separated by |||,
+-- which denotes layout choice.
+--
+myLayoutHook = onWorkspace "5:chat" imLayout $
                onWorkspace "2:web" webLayout $
                onWorkspace "6:gimp" gimpLayout $
                layoutHook defaultConfig
-  where imLayout = desktopLayoutModifiers $ withIM (1%5) imRoster Grid ||| Full
+  where imLayout = withIM (1%5) imRoster Grid ||| Full
         imRoster = ClassName "Pidgin" `And` Role "buddy_list"
-        webLayout = desktopLayoutModifiers $ Full ||| Tall 1 (3%100) (1%2)
-        gimpLayout = desktopLayoutModifiers $ withIM (0.20) (Role "gimp-toolbox") $
+        webLayout = Full ||| Tall 1 (3%100) (1%2)
+        gimpLayout = withIM (0.20) (Role "gimp-toolbox") $
                      reflectHoriz $
                      withIM (0.20) (Role "gimp-dock") Full
 
+-- | Run xmonad with a status bar.
+--
+-- The binding uses the XMonad.Hooks.ManageDocks module to automatically
+-- handle screen placement for xmobar, and enables 'mod-b' for toggling
+-- the menu bar.
+--
+myXmobar conf = statusBar "xmobar" myPP toggleStrutsKey conf
+  where
+  toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b)
+  myPP = defaultPP { ppCurrent  = wrap "[" "]" . xmobarColor "#268BD2" ""
+                   , ppVisible  = wrap "[" "]" . xmobarColor "#2AA198" ""
+                   , ppHidden   = pad . xmobarColor "#93A1A1" ""
+                   , ppHiddenNoWindows = const ""
+                   , ppUrgent   = pad . xmobarColor "#DC322F" ""
+                   , ppWsSep    = ""
+                   , ppLayout   = xmobarColor "#CB4B16" "" .
+                                  (\ x -> case x of
+                                            "TilePrime Horizontal" -> "TTT"
+                                            "TilePrime Vertical"   -> "[]="
+                                            "Hinted Full"          -> "[ ]"
+                                            _                      -> x
+                                  )
+                   , ppTitle    = xmobarColor "#268BD2" "" . shorten 60
+                   }
 
 myConfig = defaultConfig
     { terminal           = "urxvt"
     , modMask            = myModMask
     , borderWidth        = 3
     , workspaces         = myWorkspaces
-    , manageHook         = manageDocks <+> myManageHook <+> manageHook defaultConfig
+    , manageHook         = myManageHook <+> manageHook defaultConfig
     , normalBorderColor  = "#eee8d5"
     , focusedBorderColor = "#268BD2"
     , layoutHook         = myLayoutHook
@@ -104,4 +128,4 @@ myConfig = defaultConfig
 
 ---------------------------------
 -- Main function
-main = xmonad =<< xmobar myConfig
+main = xmonad =<< myXmobar myConfig
